2dea9424f6679d9a53de331745651addb7da7e62
Nikhil Kathole <nikhilkathole2683@gmail.com>
2017-03-27 11:11:17 +0000

    Prob6.py

--- solutions/prob6.py
+++ solutions/prob6.py
@@ -2,33 +2,28 @@
 # Sum square difference of first 100 natural numbers
 
 import math
-
-class sumSquareDiff(object):
+class SumSquareDiff(object):
 	
-	def __init__(self, number):
-        	self.number = number
+	def __init__(self, endrange):
+        	self.endrange = endrange
 
-	def square(self, no):
+	def cal_square(self, no):
         	return no*no
 
-	def sum(self):
-		sum1=0
-		N = self.number
-        	for i in range(1,N+1):
-			sum1=sum1+i
-		return sum1
+	def sum_upto_endrange(self):
+		temp=0
+        	for i in range(1,self.endrange+1):
+			temp += i
+		return temp
 	
-   	def sumofsquares(self):
-		sumofsquare=0
-        	N = self.number
-        	for i in range(1,N+1):
-			sumofsquare=sumofsquare+ self.square(i)
-		return sumofsquare
+   	def sum_of_squares(self):
+		sum_square=0
+        	for i in range(1,self.endrange+1):
+			sum_square=sum_square+ self.cal_square(i)
+		return sum_square
 
-		
 	def diff(self):
-		print(self.square(self.sum())-self.sumofsquares())
+		return self.cal_square(self.sum_upto_endrange())-self.sum_of_squares()
 		
-
-ob = sumSquareDiff(100)
-ob.diff()
+ob = SumSquareDiff(100)
+print(ob.diff())
